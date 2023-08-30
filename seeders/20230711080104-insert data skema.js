462a14726868f20ac304801f15bfeca642695d8e
bellawatsonf <bellawatsonf@gmail.com>
2023-08-30 16:12:51 +0700

    benerin seeder asesor

--- seeders/20230711080104-insert data skema.js
+++ seeders/20230711080104-insert data skema.js
@@ -3,74 +3,80 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.bulkInsert(
-      "Skemas",
-      [
-        // {
-        //   no_skema: "dddsds22",
-        //   nama_skema:
-        //     "Teknisi Perpajakan Pph Badan Sektor Usaha Jasa Dan Perdagangan",
-        // createdAt: new Date(),
-        // updatedAt: new Date()
-        // },
-        {
-          no_skema: "dddsds22",
-          nama_skema: "Pengelolaan Sistem Pergudangan",
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          no_skema: "dddsds22",
-          nama_skema:
-            "PKKNI Level V Pada Kompetensi Keahlian Kewirausahaan Industri",
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          no_skema: "dddsds22",
-          nama_skema: "Penyelia Ekspor",
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          no_skema: "dddsds22",
-          nama_skema: "Asisten Produser Produksi Program Televisi",
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          no_skema: "dddsds22",
-          nama_skema: "Analis Kebijakan Publik",
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          no_skema: "dddsds22",
-          nama_skema: "Manager Logistik Proyek",
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          no_skema: "dddsds22",
-          nama_skema: "Public Relations Coordinator",
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          no_skema: "dddsds22",
-          nama_skema: "Front Office Supervisor",
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          no_skema: "dddsds22",
-          nama_skema: "Media Planning Manajer",
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-      ],
-      {}
-    );
+    await queryInterface.bulkInsert("Skemas", [
+      {
+        no_skema: "dddsds22",
+        nama_skema:
+          "Teknisi Perpajakan Pph Badan Sektor Usaha Jasa Dan Perdagangan",
+        kuota: "10",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        no_skema: "dddsds22",
+        nama_skema: "Pengelolaan Sistem Pergudangan",
+        kuota: "10",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        no_skema: "dddsds22",
+        nama_skema:
+          "PKKNI Level V Pada Kompetensi Keahlian Kewirausahaan Industri",
+        kuota: "10",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        no_skema: "dddsds22",
+        nama_skema: "Penyelia Ekspor",
+        kuota: "10",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        no_skema: "dddsds22",
+        nama_skema: "Asisten Produser Produksi Program Televisi",
+        kuota: "10",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        no_skema: "dddsds22",
+        nama_skema: "Analis Kebijakan Publik",
+        kuota: "10",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        no_skema: "dddsds22",
+        nama_skema: "Manager Logistik Proyek",
+        kuota: "10",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        no_skema: "dddsds22",
+        nama_skema: "Public Relations Coordinator",
+        kuota: "10",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        no_skema: "dddsds22",
+        nama_skema: "Front Office Supervisor",
+        kuota: "10",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        no_skema: "dddsds22",
+        nama_skema: "Media Planning Manajer",
+        kuota: "10",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
     /**
      * Add seed commands here.
      *
