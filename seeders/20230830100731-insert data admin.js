99079d5ac6435c0cc548986461d029c26e7f7617
bellawatsonf <bellawatsonf@gmail.com>
2023-08-30 17:12:52 +0700

    add seeder admin

---
+++ seeders/20230830100731-insert data admin.js
@@ -0,0 +1,36 @@
+"use strict";
+
+const { encrypt } = require("../middleware/bcrypt");
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert("Admins", [
+      {
+        name: "John Doe",
+        email: "test@gmail.com",
+        password: encrypt("admin"),
+        role: "admin",
+      },
+    ]);
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("Admins", null, {});
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
